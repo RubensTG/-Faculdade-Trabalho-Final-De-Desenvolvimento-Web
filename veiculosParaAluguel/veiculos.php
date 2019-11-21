9bf26695101fcec1562ea30f1cbe42563ef7f270
RubinhoTG <rubinhotg@live.com>
2019-11-21 08:06:49 -0300

    Tela de cadastro de veículos pronta

---
+++ veiculosParaAluguel/veiculos.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Veiculos</title>
+	<html lang="pt-br">
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="veiculos.css">
+</head>
+<body>
+	<header id="cabecalho">
+		<h1 class="locadora">LOCADORA TG</h1>
+		<ul class="abas">
+			<a href="../cadastroCarro/cadastroCarro.php"><li class="aluguel">Aluguel de carros</li></a>
+			<a href="https://www.youtube.com"><li>Histórico</li></a>
+			<a href="../cadastroCliente/cadastroCliente.php"><li class="cliente">Clientes</li></a>
+		</ul>
+	</header>
+	<main>
+		<div>
+			<h1>Veiculos</h1>
+		</div>
+		<div id="divBusca">
+			<input type="text" id="txtBusca" placeholder="Buscar..."/>
+			<img src="magnifying-glass.png" id="btnBusca" alt="Buscar"/>
+		</div>
+	</main>
+
+	<footer><!--Este é um rodapé-->
+		<pre>
+			rubinhotg@live.com
+			17/11/2019
+			Todos os direitos reservados 
+			Criado por Rubens
+		</pre>
+	</footer>
+</body>
+</html>
